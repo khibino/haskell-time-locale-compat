25e820998833a8248a3e6cfabe5e3fe3dd97368d
Kei Hibino <ex8k.hibino@gmail.com>
2014-12-16 00:21:28 +0900

    Merge modules to avoid to make broken soruce archive.

--- src/Data/Time/Locale/Compat.hs
+++ src/Data/Time/Locale/Compat.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP #-}
+
 -- |
 -- Module      : Data.Time.Locale.Compat
 -- Copyright   : 2014 Kei Hibino
@@ -11,4 +13,8 @@
 -- for TimeLocale of old-locale or time-1.5.
 module Data.Time.Locale.Compat (TimeLocale, defaultTimeLocale) where
 
+#if MIN_VERSION_time(1,5,0)
+import Data.Time.Format (TimeLocale, defaultTimeLocale)
+#else
 import System.Locale (TimeLocale, defaultTimeLocale)
+#endif
